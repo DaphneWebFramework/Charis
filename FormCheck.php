e2a4724c12f1e811a90a2658a879f941968dbfc9
eylemugurel <eylemugurel@hotmail.com>
2024-12-11 18:42:53 +0100

    Add FormCheckable class

--- FormCheck.php
+++ FormCheck.php
@@ -15,72 +15,21 @@ namespace Charis;
 /**
  * Represents a Bootstrap Check component.
  *
- * This component supports the following pseudo attributes in its constructor:
- * - `:id`: A unique identifier for the input element. If omitted, a generated
- *   ID will be used if `:label-text` is provided.
- * - `:label-text`: The text content for the associated label. If present, a
- *   `<label>` element will be rendered.
- * - `:help-text`: Additional descriptive text. If present, a `<div>` element
- *   will be rendered.
- * - `:checked`: A boolean indicating whether the input should be checked.
- *   Defaults to `false`.
- * - `:disabled`: A boolean indicating whether the input should be disabled.
- *   Defaults to `false`.
- *
- * Standard HTML attributes (e.g., `class`, `style`, `data-*`) can also be
- * passed in the `$attributes` parameter and will be merged with the component's
- * default attributes.
- *
  * @link https://getbootstrap.com/docs/5.3/forms/checks-radios/#checks
  */
-class FormCheck extends Component
+class FormCheck extends FormCheckable
 {
-    /**
-     * Constructs a new instance.
-     *
-     * @param array<string, bool|int|float|string>|null $attributes
-     *   (Optional) An associative array of HTML attributes and pseudo
-     *   attributes.
-     */
-    public function __construct(?array $attributes = null)
+    #region FormCheckable overrides --------------------------------------------
+
+    protected function getWrapperClassAttribute(): string
     {
-        $id = ComponentHelper::ConsumePseudoAttribute($attributes, ':id');
-        $labelText = ComponentHelper::ConsumePseudoAttribute($attributes, ':label-text');
-        $helpText = ComponentHelper::ConsumePseudoAttribute($attributes, ':help-text');
-        $checked = ComponentHelper::ConsumePseudoAttribute($attributes, ':checked', false);
-        $disabled = ComponentHelper::ConsumePseudoAttribute($attributes, ':disabled', false);
-
-        if ($id === null && $labelText !== null) {
-            $id = 'form-check-' . \uniqid();
-        }
-
-        $helpId = $helpText !== null ? 'help-text-' . \uniqid() : null;
-
-        $content = [
-            new FormCheckInput([
-                ...($id !== null ? ['id' => $id] : []),
-                ...($helpId !== null ? ['aria-describedby' => $helpId] : []),
-                'checked' => $checked,
-                'disabled' => $disabled
-            ])
-        ];
-        if ($labelText !== null) {
-            $content[] = new FormCheckLabel(['for' => $id], $labelText);
-        }
-        if ($helpText !== null) {
-            $content[] = new FormText(['id' => $helpId], $helpText);
-        }
-
-        parent::__construct($attributes, $content);
+        return 'form-check';
     }
 
-    protected function getTagName(): string
+    protected function createFormInputComponent(array $attributes): FormInput
     {
-        return 'div';
+        return new FormCheckInput($attributes);
     }
 
-    protected function getDefaultAttributes(): array
-    {
-        return ['class' => 'form-check'];
-    }
+    #endregion FormCheckable overrides
 }
---
+++ FormCheckable.php
@@ -0,0 +1,112 @@
+<?php declare(strict_types=1);
+/**
+ * FormCheckable.php
+ *
+ * (C) 2024 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Charis;
+
+/**
+ * Abstract base class for checkable form components such as `FormCheck`,
+ * `FormRadio`, and `FormSwitch`.
+ *
+ * This component supports the following pseudo attributes in its constructor:
+ *
+ * - `:id`: A unique identifier for the input element. If omitted and a
+ *   `:label-text` is provided, an ID is generated automatically.
+ * - `:label-text`: Text for the associated `<label>` element. If omitted, no
+ *   label is rendered.
+ * - `:help-text`: Additional descriptive text. If provided, a `<div>` element
+ *   with the "form-text" class is rendered.
+ * - `:checked`: Boolean indicating whether the input should be checked.
+ *   Defaults to `false`.
+ * - `:disabled`: Boolean indicating whether the input should be disabled.
+ *   Defaults to `false`.
+ *
+ * @link https://getbootstrap.com/docs/5.3/forms/checks-radios/
+ *
+ * @codeCoverageIgnore
+ */
+abstract class FormCheckable extends Component
+{
+    /**
+     * Constructs a new instance.
+     *
+     * @param array<string, bool|int|float|string>|null $attributes
+     *   (Optional) An associative array of HTML attributes and pseudo
+     *   attributes.
+     */
+    public function __construct(?array $attributes = null)
+    {
+        $id = ComponentHelper::ConsumePseudoAttribute(
+            $attributes, ':id');
+        $labelText = ComponentHelper::ConsumePseudoAttribute(
+            $attributes, ':label-text');
+        $helpText = ComponentHelper::ConsumePseudoAttribute(
+            $attributes, ':help-text');
+        $checked = ComponentHelper::ConsumePseudoAttribute(
+            $attributes, ':checked', false);
+        $disabled = ComponentHelper::ConsumePseudoAttribute(
+            $attributes, ':disabled', false);
+
+        if ($id === null && $labelText !== null) {
+            $id = 'form-checkable-' . \uniqid();
+        }
+        $helpId = $helpText !== null ? 'help-text-' . \uniqid() : null;
+
+        $content = [
+            $this->createFormInputComponent([
+                ...($id !== null ? ['id' => $id] : []),
+                ...($helpId !== null ? ['aria-describedby' => $helpId] : []),
+                'checked' => $checked,
+                'disabled' => $disabled,
+            ])
+        ];
+        if ($labelText !== null) {
+            $content[] = new FormCheckLabel(['for' => $id], $labelText);
+        }
+        if ($helpText !== null) {
+            $content[] = new FormText(['id' => $helpId], $helpText);
+        }
+
+        parent::__construct($attributes, $content);
+    }
+
+    /**
+     * Provides the CSS class for the wrapper element.
+     *
+     * @return string
+     *   The CSS class string for the wrapper element.
+     */
+    abstract protected function getWrapperClassAttribute(): string;
+
+    /**
+     * Creates the form input component.
+     *
+     * @param array<string, bool|int|float|string> $attributes
+     *   Attributes for the input component.
+     * @return FormInput
+     *   The form input component instance.
+     */
+    abstract protected function createFormInputComponent(array $attributes): FormInput;
+
+    #region Component overrides ------------------------------------------------
+
+    protected function getTagName(): string
+    {
+        return 'div';
+    }
+
+    protected function getDefaultAttributes(): array
+    {
+        return ['class' => $this->getWrapperClassAttribute()];
+    }
+
+    #endregion Component overrides
+}

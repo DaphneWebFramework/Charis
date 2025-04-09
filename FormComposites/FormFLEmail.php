f10471c7c8f96e57b8b2609f3c75e0be282acfc0
eylemugurel <eylemugurel@hotmail.com>
2025-04-09 21:05:55 +0200

    Add FormEmailInput, FormEmail, FormFLEmail

---
+++ FormComposites/FormFLEmail.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/**
+ * FormFLEmail.php
+ *
+ * (C) 2024 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Charis\FormComposites;
+
+use \Charis\FormControls\FormInput;
+use \Charis\FormControls\FormEmailInput;
+
+/**
+ * Represents an email input with a floating label and optional help text.
+ */
+class FormFLEmail extends FormFloatingLabelComposite
+{
+    #region FormComposite overrides --------------------------------------------
+
+    protected function createFormInputComponent(array $attributes): FormInput
+    {
+        return new FormEmailInput($attributes);
+    }
+
+    #endregion FormComposite overrides
+}
---
+++ FormControls/FormEmailInput.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+/**
+ * FormEmailInput.php
+ *
+ * (C) 2024 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Charis\FormControls;
+
+/**
+ * Represents a email input.
+ */
+class FormEmailInput extends FormInput
+{
+    #region Component overrides ------------------------------------------------
+
+    protected function getDefaultAttributes(): array
+    {
+        return \array_merge(parent::getDefaultAttributes(), [
+            'type' => 'email'
+        ]);
+    }
+
+    #endregion Component overrides
+}

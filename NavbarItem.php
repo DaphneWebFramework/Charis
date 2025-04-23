907ea744fa291a3a0f3efaed887dfa833d09d886
eylemugurel <eylemugurel@hotmail.com>
2025-04-23 22:04:28 +0200

    Add NavbarNav and NavbarItem

---
+++ NavbarItem.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+/**
+ * NavbarItem.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Charis;
+
+/**
+ * Represents a single navigation item inside a navbar.
+ *
+ * Aside from HTML attributes that apply to the wrapper element, this component
+ * supports the following pseudo attributes in its constructor:
+ *
+ * - `:label`: The text for the link.
+ * - `:href`: The URL for the link.
+ * - `:active`: Boolean indicating whether the link represents the current page.
+ * - `:disabled`: Boolean indicating whether the link is disabled.
+ */
+class NavbarItem extends Component
+{
+    /**
+     * Constructs a new instance.
+     *
+     * @param ?array<string, mixed> $attributes
+     *   (Optional) An associative array where standard HTML attributes apply to
+     *   the wrapper element, and pseudo attributes configure inner components.
+     *   Pass `null` or an empty array to indicate no attributes. Defaults to
+     *   `null`.
+     */
+    public function __construct(?array $attributes = null)
+    {
+        $label = $this->consumePseudoAttribute($attributes, ':label', '');
+        $href = $this->consumePseudoAttribute($attributes, ':href', '#');
+        $active = $this->consumePseudoAttribute($attributes, ':active', false);
+        $disabled = $this->consumePseudoAttribute($attributes, ':disabled', false);
+
+        $linkAttributes = [
+            'class' => 'nav-link',
+            'href' => $href
+        ];
+        if ($active) {
+            $linkAttributes['class'] .= ' active';
+            $linkAttributes['aria-current'] = 'page';
+        }
+        if ($disabled) {
+            $linkAttributes['class'] .= ' disabled';
+            $linkAttributes['aria-disabled'] = 'true';
+        }
+        $content = new Generic('a', $linkAttributes, $label);
+
+        parent::__construct($attributes, $content);
+    }
+
+    #region Component overrides ------------------------------------------------
+
+    protected function getTagName(): string
+    {
+        return 'li';
+    }
+
+    protected function getDefaultAttributes(): array
+    {
+        return ['class' => 'nav-item'];
+    }
+
+    #endregion Component overrides
+}
---
+++ NavbarNav.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/**
+ * NavbarNav.php
+ *
+ * (C) 2025 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+namespace Charis;
+
+/**
+ * Represents a container for navigation items inside a navbar.
+ *
+ * @link https://getbootstrap.com/docs/5.3/components/navbar/#nav
+ */
+class NavbarNav extends Component
+{
+    #region Component overrides ------------------------------------------------
+
+    protected function getTagName(): string
+    {
+        return 'ul';
+    }
+
+    protected function getDefaultAttributes(): array
+    {
+        return ['class' => 'navbar-nav'];
+    }
+
+    #endregion Component overrides
+}
